145bb1d53339d5db7fbaa3d871c992b65709996e
HyeonDobin <hyeondobin@github.com>
2023-10-23 20:28:18 +0900

    test commit
    test

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log(0.1 + 0.2);
+let weight;
+let foodWeight;
+
+function printName() {
+    const name = document.getElementById("weight").value;
+    document.getElementById("result").innerText = name;
+}
